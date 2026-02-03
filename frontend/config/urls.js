26733c99ff9fdbb880add4d54e1ae33879311221
NavserX <snavarroazorin@gmail.com>
2026-02-03 21:12:03 +0100

    Corregida URL de API para local y produccion

--- frontend/config/urls.js
+++ frontend/config/urls.js
@@ -2,4 +2,13 @@
 /*let apiUrl = "";
 var BACKEND_URL = "http://localhost:8080";
 */
-const apiUrl = "http://localhost:8080";
+//const apiUrl = "http://localhost:8080";
+
+const apiUrl = window.location.hostname === "localhost" 
+    ? "http://localhost:8080" 
+    : "http://51.20.1.231:8080";
+
+    
+fetch(`${apiUrlL}/tu_script_backend.php`)
+  .then(res => res.json())
+  .then(data => console.log(data));
